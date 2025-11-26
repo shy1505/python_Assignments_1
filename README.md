b7df6b39f00537570ed42fe8c0cf5495954d5ac0
shy1505 <kumarishyamali1505@gmail.com>
2025-11-27 01:38:35 +0530

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # python_Assignments_1
-First assignment of tutedude to be ib python language
+First assignment of tutedude to be into python language
---
+++ assignment_1b.py
@@ -0,0 +1,4 @@
+user_first_name=input("Enter your first name: ")
+user_last_name=input("Enter your last name: ")
+
+print("Hello, ",user_first_name,user_last_name,"! Welcome to the Python program.")

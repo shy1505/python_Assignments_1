5bdebb05521401debfb18e68356bf03c87744ad9
shy1505 <kumarishyamali1505@gmail.com>
2025-11-27 01:38:35 +0530

    initial commit

---
+++ assignment_1.py
@@ -0,0 +1,15 @@
+# Perform Basic Operations like addition,subtraction,multiplication,divide by taking two number as user input
+num1=float(input("Enter the first number: "))
+num2=float(input("Enter the second number: "))
+
+Addition=int(num1)+int(num2)
+Subtraction=int(num1)-int(num2)
+Multiplication=int(num1)*int(num2)
+Division=int(num1)/int(num2)
+
+print("Addition: ",Addition)
+print("Subtraction: ",Subtraction)
+print("Multiplication: ",Multiplication)
+print("Division: ",round(Division,2))
+
+
